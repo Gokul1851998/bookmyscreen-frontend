41a175750d101fabb20b2dc6d2117cad1d3e9535
gokul <gokulmjp@gmail.com>
2023-05-08 14:07:17 +0530

    next10

--- apiLinks/apiLinks.js
+++ apiLinks/apiLinks.js
@@ -1,13 +1,13 @@
-export const userUrl='http://localhost:3000/' 
-export const ownerUrl = 'http://localhost:3000/owner/'
-export const adminUrl ='http://localhost:3000/admin/'
-export const movieUrl ='https://api.themoviedb.org/3'
-
-
-// export const userUrl='https://bookmyscreen.onrender.com/' 
-// export const ownerUrl = 'https://bookmyscreen.onrender.com/owner/'
-// export const adminUrl ='https://bookmyscreen.onrender.com/admin/'
+// export const userUrl='http://localhost:3000/' 
+// export const ownerUrl = 'http://localhost:3000/owner/'
+// export const adminUrl ='http://localhost:3000/admin/'
 // export const movieUrl ='https://api.themoviedb.org/3'
+
+
+export const userUrl='https://bookmyscreen.onrender.com/' 
+export const ownerUrl = 'https://bookmyscreen.onrender.com/owner/'
+export const adminUrl ='https://bookmyscreen.onrender.com/admin/'
+export const movieUrl ='https://api.themoviedb.org/3'
 
 
 
