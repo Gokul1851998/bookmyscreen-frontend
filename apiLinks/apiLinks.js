cb7daf180507af6153cf0c1d4ee9d5ad90acd18e
gokul <gokulmjp@gmail.com>
2023-05-11 19:47:02 +0530

    last

--- apiLinks/apiLinks.js
+++ apiLinks/apiLinks.js
@@ -1,18 +1,18 @@
-// export const userUrl='http://localhost:3000/' 
-// export const ownerUrl = 'http://localhost:3000/owner/'
-// export const adminUrl ='http://localhost:3000/admin/'
+// export const userUrl='http://localhost:2000/' 
+// export const ownerUrl = 'http://localhost:2000/owner/'
+// export const adminUrl ='http://localhost:2000/admin/'
 
 
 
-export const userUrl='https://bookmyscreen.onrender.com/' 
-export const ownerUrl = 'https://bookmyscreen.onrender.com/owner/'
-export const adminUrl ='https://bookmyscreen.onrender.com/admin/'
+// export const userUrl='https://bookmyscreen.onrender.com/' 
+// export const ownerUrl = 'https://bookmyscreen.onrender.com/owner/'
+// export const adminUrl ='https://bookmyscreen.onrender.com/admin/'
 
 
 
-// export const userUrl='https://echo-cart.shop/' 
-// export const ownerUrl = 'https://echo-cart.shop/owner/'
-// export const adminUrl ='https://echo-cart.shop/admin/'
+export const userUrl='https://echo-cart.shop/' 
+export const ownerUrl = 'https://echo-cart.shop/owner/'
+export const adminUrl ='https://echo-cart.shop/admin/'
 
 
 export const movieUrl ='https://api.themoviedb.org/3'
